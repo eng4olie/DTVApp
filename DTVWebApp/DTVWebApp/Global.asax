165a885c368bc5d72820290b2b41e99ad43bf247
mikeciav <mikeciav@gmail.com>
2015-11-21 16:41:01 -0800

    no comment

---
+++ DTVWebApp/DTVWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DTVWebApp.MvcApplication" Language="C#" %>
---
+++ DTVWebApp/DTVWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DTVWebApp.Startup))]
+namespace DTVWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
